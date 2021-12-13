6d416a8cb4be31908a3cd1e469fe95596b096c31
sungsoo_park <sungsoo.park@voithru.com>
2021-12-13 10:38:50 +0900

    feat: Add inference code

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import typer
 
 from train import train
+from predict import predict
 
 app = typer.Typer()
 app.command(name="train")(train)
+app.command(name="predict")(predict)
 
 if __name__ == "__main__":
     app()
